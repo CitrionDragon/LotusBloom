6af024a8504e692e39ffba8e8eba7c201677f1c6
CitrionDragon <143145263+CitrionDragon@users.noreply.github.com>
2024-09-09 08:33:39 -0600

    Update SampleRoleAddon.cs

--- SampleRoleAddon.cs
+++ SampleRoleAddon.cs
@@ -15,7 +15,7 @@ public class SampleRoleAddon: LotusAddon
     public override void Initialize()
     {
         // Create instances first
-        List<CustomRole> allRoles = new List<CustomRole>() { new CrewCrew() , new Hypnotist() };
+        List<CustomRole> allRoles = new List<CustomRole>() { new CrewCrew() , new Hypnotist() ,new Harbinger()};
 
         // Add your role to the gamemmode of your choice (Standard in this case.)
         allRoles.ForEach(StandardRoles.AddRole);
