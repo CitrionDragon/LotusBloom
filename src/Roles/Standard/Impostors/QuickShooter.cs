5f2e2915118297828e53f1d51e45a7912b7b9167
CitrionDragon <obsidiancreeperdragon@gmail.com>
2024-09-15 16:39:43 -0600

    Create QuickShooter.cs

---
+++ src/Roles/Standard/Impostors/QuickShooter.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using System.Linq;
+using Lotus.API.Odyssey;
+using Lotus.API.Player;
+using Lotus.Factions;
+using Lotus.GUI;
+using Lotus.GUI.Name;
+using Lotus.Options;
+using Lotus.Roles.Internals.Attributes;
+using Lotus.Roles.Internals.Enums;
+using Lotus.Roles.Overrides;
+using Lotus.Roles.RoleGroups.Vanilla;
+using UnityEngine;
+using VentLib.Options.UI;
+using VentLib.Options.IO;
+using VentLib.Utilities;
+using VentLib.Utilities.Collections;
+using VentLib.Utilities.Extensions;
+using Lotus.Extensions;
+
+namespace LotusBloom.Roles.Standard.Impostors
+
+public class QuickShooter : Impostor
+{
+    private int maxBullets;
+    private int bulletCount;
+    private int keptBullets;
+    private float reloadCooldown;
+
+    [RoleAction(LotusActionType.Attack)]
+    public override bool TryKill(PlayerControl target)
+    {
+        reloadCooldown = KillCooldown;
+        if (bulletCount == 0) return true;
+        cooldownOverride = AddOverride(new GameOptionOverride(Override.KillCooldown, 1));
+        bulletCount--;
+        return true;
+    }
+
+    [RoleAction(LotusActionType.OnPet)]
+    public void GainBullet()
+    {
+        if (reloadCooldown >= 0 || bulletCount == maxBullets) return false;
+        reloadCooldown = KillCooldown;
+        bulletCount++;
+        MyPlayer.RpcMark(MyPlayer);
+    }
+
+    [RoleAction(LotusActionType.RoundStart)]
+    public void ResetBullets()
+    {
+        bulletCount = keptBullets
+    }
+    protected override GameOptionBuilder RegisterOptions(GameOptionBuilder optionStream) =>
+        base.RegisterOptions(optionStream)
+            .SubOption(sub => sub.Name("Max Bullets")
+                .AddIntRange(1, 15, 1, 5)
+                .BindInt(i => maxBullets = i)
+                .ShowSubOptionPredicate(o => (int)o > 1)
+                .Build())
+            .SubOption(sub => sub.Name("Kept Bullets in Meeting")
+                .AddIntRange(1, 15, 1, 5)
+                .BindInt(i => keptBullets = i)
+                .ShowSubOptionPredicate(o => (int)o > 1)
+                .Build());
+
+}
