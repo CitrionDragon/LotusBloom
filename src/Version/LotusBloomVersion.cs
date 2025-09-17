3bdd5fb07a256c090733e48b3c9ed9cd2e31e0bb
CitrionDragon <obsidiancreeperdragon@gmail.com>
2025-09-17 10:35:07 -0600

    Update LotusBloomVersion.cs

--- src/Version/LotusBloomVersion.cs
+++ src/Version/LotusBloomVersion.cs
@@ -19,7 +19,7 @@ public class LotusBloomVersion : VentLib.Version.Version
 
     public override string ToSimpleName()
     {
-        return "Lotus Bloom Addon Version v1.1.2";
+        return "Lotus Bloom Addon Version v1.2.0";
     }
 
     public override string ToString() => "LotusBloomAddon";
